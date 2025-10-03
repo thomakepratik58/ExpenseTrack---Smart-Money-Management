9f40bd0362bedb9378b26b4b2b792e436116cc20
pratik thomake <thomakepratik58@gmail.com>
2025-10-03 23:55:08 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
+  // This base path is set to match your GitHub repository name
+  base: "/ExpenseTrack---Smart-Money-Management/", 
   plugins: [react()],
 })
